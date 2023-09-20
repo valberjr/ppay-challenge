4299f9a47bde646705a89783e2876166237125bc
valberjr <vjunior@skiff.com>
2023-09-20 16:21:40 -0300

    first commit

---
+++ src/test/java/com/example/ppay/PpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ppay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
