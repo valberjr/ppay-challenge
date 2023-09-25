289f7a859cfdeeaf75bec82b3e3e8db172affc9d
valberjr <vjunior@skiff.com>
2023-09-25 10:48:46 -0300

    feat: account service unit test

--- src/main/java/com/example/ppay/model/Account.java
+++ src/main/java/com/example/ppay/model/Account.java
@@ -1,10 +1,7 @@
 package com.example.ppay.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -15,6 +12,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Account {
 
     @Id
---
+++ src/test/java/com/example/ppay/service/AccountServiceTest.java
@@ -0,0 +1,104 @@
+package com.example.ppay.service;
+
+import com.example.ppay.dto.AccountDto;
+import com.example.ppay.dto.mapper.AccountMapper;
+import com.example.ppay.exception.BalanceException;
+import com.example.ppay.model.Account;
+import com.example.ppay.repository.AccountRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AccountServiceTest {
+
+    @Mock
+    AccountRepository repository;
+    @Mock
+    AccountMapper mapper;
+    @InjectMocks
+    AccountService service;
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @DisplayName("Should return an empty list when no accounts exist")
+    @Test
+    void shouldReturnEmptyListWhenNoAccountsExist() {
+        given(repository.findAll()).willReturn(Collections.emptyList());
+        assertThat(service.findAll()).isEmpty();
+    }
+
+    @DisplayName("Should throw EntityNotFoundException when user with given id does not exist")
+    @Test
+    void shouldThrowEntityNotFoundExceptionWhenUserWithGivenIdDoesNotExist() {
+        String userId = "3263ee01-4669-49ef-8bae-b830cf7918f5";
+        when(repository.findByUserId(userId)).thenReturn(Optional.empty());
+        assertThrows(EntityNotFoundException.class, () -> service.findByUserId(userId));
+    }
+
+    @DisplayName("Should update an account balance subtracting the amount")
+    @Test
+    void shouldUpdateAccountBalanceSubtractingAmount() {
+        AccountDto accountDto = new AccountDto("1253ff02", 1, BigDecimal.ZERO, null);
+        Account account = Account.builder()
+                .id("1253ff02")
+                .number(1)
+                .balance(new BigDecimal("100"))
+                .build();
+
+        boolean isSender = true;
+
+        when(mapper.toEntity(accountDto)).thenReturn(account);
+        when(repository.save(any())).thenReturn(account);
+
+        service.updateAccountBalance(accountDto, new BigDecimal("50"), isSender);
+
+        assertThat(account.getBalance()).isEqualTo(new BigDecimal("50"));
+    }
+
+    @DisplayName("Should update an account balance adding the amount")
+    @Test
+    void shouldUpdateAccountBalanceAddingAmount() {
+        AccountDto accountDto = new AccountDto("1253ff02", 1, BigDecimal.ZERO, null);
+        Account account = Account.builder()
+                .id("1253ff02")
+                .number(1)
+                .balance(new BigDecimal("100"))
+                .build();
+
+        boolean isSender = false;
+
+        when(mapper.toEntity(accountDto)).thenReturn(account);
+        when(repository.save(any())).thenReturn(account);
+
+        service.updateAccountBalance(accountDto, new BigDecimal("50"), isSender);
+
+        assertThat(account.getBalance()).isEqualTo(new BigDecimal("150"));
+    }
+
+    @DisplayName("Should throws BalanceException if has no balance")
+    @Test
+    void shouldThrowsBalanceExceptionIfHasNoBalance() {
+        AccountDto accountDto = new AccountDto("1253ff02", 1, BigDecimal.ZERO, null);
+        assertThrows(BalanceException.class, () -> service.hasEnoughBalance(accountDto));
+    }
+
+}
